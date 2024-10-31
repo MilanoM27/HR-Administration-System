789b85ff5bf49052fa69d235add45b12eb9db128
MilanoM <60301624+MilanoM27@users.noreply.github.com>
2024-10-31 09:01:33 +0200

    updated file structure

--- hradmin/src/pages/api/auth/login.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../app/_components/LoginForm";
-
-const LoginPage = () => (
-  <div className="flex items-center justify-center h-screen bg-gray-100">
-    <LoginForm />
-  </div>
-);
-
-export default LoginPage;
